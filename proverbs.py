17f5dda26f63484ed43355d9f687f165e77b13ad
Stefanos Kiourkoulis <kiourkoulis.stefanos@gmail.com>
2015-09-08 19:54:08 +0300

    Initial commit, storing purposes.

---
+++ proverbs.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+import xml.etree.ElementTree as ET
+from sys import exit
+from random import choice
+from urllib2 import urlopen
+
+def random_proverbs():
+    proverbs = []
+    url = "http://stefki.com/sandbox/python/greek_proverbs/proverbs.xml"
+
+    tree = ET.parse( urlopen( url ) )
+    root = tree.getroot()
+
+    for proverb in root.findall( 'proverb' ):
+        proverbs.append( proverb.text )
+ 
+    return ( choice(proverbs).split(",")[0], choice(proverbs).split(",")[1] )
+ 
+def main():
+    print "\"%s, %s\"" % random_proverbs()
+    return 0
+
+if __name__ == "__main__":
+    exit( main() )
